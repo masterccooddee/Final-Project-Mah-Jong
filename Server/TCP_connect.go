d451fc4ee83fb121529f7c230c3c8572c3339cca
masterccooddee <arthurhmy@gmail.com>
2024-12-03 13:43:14 +0800

    Fix welcome message in TCP_connect.go and add new player to playerlist.txt

--- Server/TCP_connect.go
+++ Server/TCP_connect.go
@@ -329,7 +329,7 @@ func startserver() {
 				return
 
 			}
-			//conn.Write([]byte("Welcome to the server\n"))
+			conn.Write([]byte("Welcome to the server"))
 			go Cli_handle(conn, player_in{}, ctx)
 		}
 
